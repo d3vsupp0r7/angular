00b22de83ec2556296e4d5c8b7385bea3aea6a7f
Aniello La Bua <aniello.labua@alliance-healthcare.net>
2019-08-20 17:31:45 +0200

    First commit

--- angular-hello-world-app/src/app/employee.service.ts
+++ angular-hello-world-app/src/app/employee.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+//
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  //
+  private baseUrl = 'http://localhost:8080/employee';
+
+  constructor(private http: HttpClient) { }
 }
