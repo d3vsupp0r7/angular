9550e0557c0b0d3a188d33a20ac1e3d4f2dd709c
d3vsupp0r7 <dev@dev.com>
2019-08-21 20:49:44 +0200

    Template-Driven-Form: SelectBox example

---
+++ angular-hello-world-app/src/app/data/address.model.ts
@@ -0,0 +1,9 @@
+// Simple model for template-driven-form
+export interface addressModel{  
+    address: string,  
+    city: string,  
+    state: string,  
+    postcode: number,  
+    country: any[],  
+    aggrement: boolean  
+}  
--- angular-hello-world-app/src/app/template-driven-form-example/template-driven-form-example.component.ts
+++ angular-hello-world-app/src/app/template-driven-form-example/template-driven-form-example.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 //
 import { FormsModule } from '@angular/forms';
+//
+import { addressModel } from 'src/app/data/address.model';  
 
 @Component({
   selector: 'app-template-driven-form-example',
@@ -8,6 +10,16 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./template-driven-form-example.component.css']
 })
 export class TemplateDrivenFormExampleComponent implements OnInit {
+
+  //Form object definition
+  model: addressModel = {  
+    address: '',  
+    city: '',  
+    state:'',  
+    postcode: null,  
+    country: null,  
+    aggrement: false  
+  };  
 
   constructor() { }
 
@@ -19,5 +31,8 @@ export class TemplateDrivenFormExampleComponent implements OnInit {
   onSubmit(form) {
     console.log('form values ', form);
   }
+
+  //
+  countryData: any[] = ['India', 'US', 'UK'];  
 
 }
