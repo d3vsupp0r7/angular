33d94cebd393108ecdf97e4cbd2162314f3d56b7
Aniello La Bua <aniello.labua@alliance-healthcare.net>
2019-08-26 13:22:44 +0200

    CREATE: Added create funtionality to CreateEmployeeComponent

--- angular-hello-world-app/src/app/create-employee/create-employee.component.ts
+++ angular-hello-world-app/src/app/create-employee/create-employee.component.ts
@@ -4,7 +4,7 @@ import { EmployeeService } from '../employee.service';
 //
 import { Employee } from '../employee';
 //
-
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -16,10 +16,38 @@ export class CreateEmployeeComponent implements OnInit {
 
   //
   employee: Employee = new Employee();
+  submitted = false;
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService,
+    private router: Router) { }
 
   ngOnInit() {
   }
 
+  //
+  newEmployee(): void {
+    this.submitted = false;
+    this.employee = new Employee();
+  }
+
+  //
+  save() {
+    this.employeeService.createEmployee(this.employee)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.employee = new Employee();
+    this.gotoList();
+  }
+  
+  //
+  onSubmit() {
+    this.submitted = true;
+    this.save();    
+  }
+
+  gotoList() {
+    this.router.navigate(['/employees']);
+  }
+
 }
+
+
