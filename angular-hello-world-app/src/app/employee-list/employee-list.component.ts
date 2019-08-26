76bca867d3468e21ced36c2531ea7cf8d47c0767
Aniello La Bua <aniello.labua@alliance-healthcare.net>
2019-08-26 13:22:07 +0200

    READ: Added Read functionality to EmployeeListComponent

--- angular-hello-world-app/src/app/employee-list/employee-list.component.ts
+++ angular-hello-world-app/src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+//
+import { Observable } from "rxjs";
+//
+import { EmployeeService } from "../employee.service";
+// Import model
+import { Employee } from "../employee";
+//
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,10 +14,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
+  //List of employees to render
+  employees: Observable<Employee[]>;
 
-  constructor() {  }
+  constructor(private employeeService: EmployeeService,
+    private router: Router) {  }
 
   ngOnInit() {
+    //
+    this.reloadData();
   }
+
+  reloadData() {
+    this.employees = this.employeeService.getEmployeesList();
+  }
+
+  //Delete
+  deleteEmployee(id: number) {
+    this.employeeService.deleteEmployee(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log(error));
+  }
+
 
 }
